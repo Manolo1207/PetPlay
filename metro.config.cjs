c06c4c77a48672c36dc3f7deb7f3d30dad2536db
Manolo1207 <salcedotrujillomanuelalejandro@gmail.com>
2026-02-23 18:25:59 -0600

    Preparar app para producción: limpiar mocks, dependencias y pantallas

--- metro.config.cjs
+++ metro.config.cjs
@@ -1,18 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Agregar soporte para alias de imports
-config.resolver.extraNodeModules = {
-    '@components': path.resolve(__dirname, 'src/components'),
-    '@screens': path.resolve(__dirname, 'src/screens'),
-    '@services': path.resolve(__dirname, 'src/services'),
-    '@hooks': path.resolve(__dirname, 'src/hooks'),
-    '@types': path.resolve(__dirname, 'src/types'),
-    '@utils': path.resolve(__dirname, 'src/utils'),
-    '@navigation': path.resolve(__dirname, 'src/navigation'),
-    '@': path.resolve(__dirname, './'),
-};
 
 module.exports = config;
